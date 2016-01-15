a8b087afc19c6ffb0d3b8011eb9e2d8b256e1b04
penguin-penpen <penguingohack@outlook.com>
2016-01-15 20:26:27 +0800

    create database

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 __author__ = 'yang'
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
-from app import views
+db = SQLAlchemy(app)
+from app import views, models
--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 __author__ = 'yang'
 from app import app
 from flask import render_template
@@ -27,10 +28,18 @@ def index():
 
 @app.route('/login', methods= ['GET', 'POST'])
 def login():
+    #创建表单
     form = LoginForm()
+
+    #提交表单的动作
     if form.validate_on_submit():
+
+        #显示信息
         flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
+        #处理完后重定向到首页
         return redirect('/index')
+    #以GET方式请求页面是返回html
     return render_template('login.html',
                            title = 'Sign In',
-                           form = form)
+                           form = form,
+                           providers = app.config['OPENID_PROVIDERS'])
