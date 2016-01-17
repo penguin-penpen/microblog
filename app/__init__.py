7327be2274b287df748a10e65cda1b235a309fb3
penguin-penpen <penguingohack@outlook.com>
2016-01-17 13:05:04 +0800

    User login, error not solved.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,16 @@
+# -*- coding: utf-8 -*-
+import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app) #create database object
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login' #Flask-Login需要知道哪个函数允许用户登录。
+oid = OpenID(app, os.path.join(basedir, 'tmp')) #Flask-OpenID扩展需要一个存储文件的临时文件夹路径
 from app import views, models
--- app/views.py
+++ app/views.py
@@ -1,15 +1,23 @@
 # -*- coding: utf-8 -*-
-__author__ = 'yang'
-from app import app
-from flask import render_template
-from flask import flash
-from flask import redirect
-from forms import LoginForm
+from flask import flash, render_template, redirect, session, url_for, request, g
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from .forms import LoginForm
+from app import app, db, lm, oid
+from .models import User
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id)) #id是unicode字符串??
+
+@app.before_request
+def before_request():
+    g.user = current_user
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = { 'nickname': 'Miguel' } # fake user
+    user = g.user
     posts = [
         {
             'author': {'nickname': 'John'},
@@ -26,20 +34,49 @@ def index():
                            user = user,
                            posts = posts)
 
+
 @app.route('/login', methods= ['GET', 'POST'])
+@oid.loginhandler
 def login():
+    if g.user is not None and g.user.is_authenticated():
+        return redirect(url_for('index'))
     #创建表单
     form = LoginForm()
 
     #提交表单的动作
     if form.validate_on_submit():
-
-        #显示信息
-        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
-        #处理完后重定向到首页
-        return redirect('/index')
+        session['remember_me'] = form.remember_me.data
+        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
     #以GET方式请求页面是返回html
     return render_template('login.html',
                            title = 'Sign In',
                            form = form,
                            providers = app.config['OPENID_PROVIDERS'])
+
+
+@oid.after_login
+def after_login(resp): #rep参数包含了从OpenID提供商返回来的信息
+    #邮箱为空
+    if resp.email is None or resp.email == '':
+        flash('Invalid login. Please try again.')
+        return redirect(url_for('login'))
+    #从数据库中搜索邮箱地址，如果邮箱地址不在数据库中，为新用户。
+    user = User.query.filter_by(email=resp.email).first()
+    if user is None:
+        nickname = resp.nickname
+        if nickname is None or nickname == '':
+            nickname = resp.email.split('@')[0]
+            user = User(nickname=nickname, email=resp.email)
+            db.session.add(user)
+            db.session.commit()
+    remember_me = False
+    if 'remember_me' in session:
+        remember_me = session['remember_me']
+        session.pop('remember_me', None)
+    login_user(user, remember=remember_me)
+    #在next页面没有提供的情况下，重定向到首页，否则重定向到next页
+    return redirect(request.args.get('next') or url_for('index'))
+
+@app.before_request
+def before_request():
+    g.user = current_user
