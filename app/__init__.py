9f82e3cc67ac7358d8c3cc0e6009a21949411dbb
penguin-penpen <penguingohack@outlook.com>
2016-01-26 23:58:56 +0800

    添加momentjs自动转换时间戳，安装bootstrap框架

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,8 @@ from flask.ext.login import LoginManager #class LoginManager
 from flask.ext.openid import OpenID #class OpenID
 from config import basedir #from module import basedir obj
 
+from momentjs import momentjs
+
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app) #create database object
@@ -13,4 +15,5 @@ lm = LoginManager()
 lm.init_app(app) #configure an application
 lm.login_view = 'login' #Flask-Login需要知道哪个函数允许用户登录。
 #oid = OpenID(app, os.path.join(basedir, 'tmp')) #Flask-OpenID扩展需要一个存储文件的临时文件夹路径
-from app import views, models #from folder import modules
+app.jinja_env.globals['momentjs'] = momentjs #告诉Jinja2导入momentjs类作为所有模板的全局变量
+from app import views, models #from folder import modules   此行不能提前到脚本开头，为什么？
--- app/views.py
+++ app/views.py
@@ -135,3 +135,7 @@ def edit():
         form.nickname.data = g.user.nickname
         form.about_me.data = g.user.about_me
     return render_template('edit.html', form = form)
+
+@app.route('/testBootstrap')
+def test():
+    return render_template('testBootstrap.html')
